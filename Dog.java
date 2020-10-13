5f1fe0f5312360507189199024943a1d09acbc7e
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-10-13 12:22:05 -0400

    inheritance article

---
+++ Dog.java
@@ -0,0 +1,14 @@
+class Animal {
+  public Animal(String name) {
+  }
+}
+
+public class Dog extends Animal {
+  private String name;
+  private String breed;
+
+    public Dog(String name, String breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+}
